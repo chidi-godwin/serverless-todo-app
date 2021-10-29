bf108e0fd30f6c32e4e076f91f616fa83cb61e1d
chidi-godwin <chidieberen1998@gmail.com>
2021-10-29 02:35:24 +0100

    implement auth

--- course-04/project/c4-final-project-starter-code/backend/serverless.ts
+++ course-04/project/c4-final-project-starter-code/backend/serverless.ts
@@ -27,21 +27,9 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-      Auth_0_SECRET_ID: 'Auth0Secret-${self:provider.stage}',
-      Auth_0_SECRET_FIELD: 'auth0Secret'
     },
     lambdaHashingVersion: '20201221',
     iamRoleStatements: [
-      {
-        Effect: 'Allow',
-        Action: ['secretsmanager:GetSecretValue'],
-        Resource: {"Ref": "Auth0Secret"}
-      },
-      {
-        Effect: 'Allow',
-        Action: ['kms:Decrypt'],
-        Resource: {"Fn::GetAtt": ["KMSKey", "Arn"]}
-      }
     ]
   },
   // import the function via paths
@@ -62,51 +50,6 @@ const serverlessConfiguration: AWS = {
           }
         }
       },
-      KMSKey: {
-        Type: 'AWS::KMS::Key',
-        Properties: {
-          Description: "KMS key to encrypt Auth0 secret",
-          KeyPolicy: {
-            Version: '2012-10-17',
-            Id: 'key-default-1',
-            Statement: [
-              {
-                Sid: 'Allow administration of the key',
-                Effect: 'Allow',
-                Principal: {
-                  AWS: {
-                    "Fn::Join": [
-                      ':',
-                      [
-                        'arn:aws:iam:',
-                        {Ref: "AWS::AccountId"},
-                        'root'
-                      ]
-                    ]
-                  }
-                },
-                Action: ['kms:*'],
-                Resource: '*'
-              }
-            ]
-          }
-        }
-      },
-      KMSKeyAlias: {
-        Type: 'AWS::KMS::Alias',
-        Properties: {
-          AliasName: 'alias/auth0Key-${self:provider.stage}',
-          TargetKeyId: {"Ref": "KMSKey"}
-        }
-      },
-      Auth0Secret: {
-        Type: "AWS::SecretsManager::Secret",
-        Properties: {
-          Name: '${self:provider.environment.AUTH_0_SECRET_ID}',
-          Description: 'Auth0 Secret',
-          KmsKeyId: {"Ref": "KMSKey"}
-        }
-      }
     }
   }
 };
